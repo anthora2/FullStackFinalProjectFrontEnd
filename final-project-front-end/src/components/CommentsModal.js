4d1bfeaa6e62ef4ca0fb634cb904550604daa1e1
Anthony Alvarez <anthonyalvarez@Anthonys-MacBook-Pro.local>
2024-04-24 22:44:34 -0700

    Most things components are ready to either receive or send to backend, have to discuss in which format it will be happening or i can set them up ahead of time

--- final-project-front-end/src/components/CommentsModal.js
+++ final-project-front-end/src/components/CommentsModal.js
@@ -1,42 +1,47 @@
-import React, { useState } from 'react';
+import React from "react";
 
-const CommentsModal = ({ isOpen, onClose }) => {
-  const fakeComments = [
-    "This website is awesome!",
-    "I found my dream home here!",
-    "Great user experience!",
-    "This website is awesome!",
-    "I found my dream home here!",
-    "Great user experience!",
-    "This website is awesome!",
-    "I found my dream home here!",
-    "Great user experience!",
-    "This website is awesome!",
-    "I found my dream home here!",
-    "Great user experience!",
-    "This website is awesome!",
-    "I found my dream home here!",
-    "Great user experience!",
-    // Add more fake comments as needed
-  ];
-
+const CommentsModal = ({ isOpen, onClose, comments }) => {
   return isOpen ? (
     <div className="fixed inset-0 z-50 flex justify-start items-center bg-gray-800 bg-opacity-50 pl-[120px] w-2/3">
       <div className="bg-white rounded-lg p-6 max-h-96 overflow-y-auto relative left-0 w-[460px]">
-        <button onClick={onClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
           </svg>
         </button>
         <h2 className="text-xl font-bold mb-4">Comments</h2>
-        <div>
-          {fakeComments.map((comment, index) => (
-            <p key={index} className="mb-2">{comment}</p>
-          ))}
-        </div>
+        {comments.map((comment, index) => (
+          <div key={index} className="flex flex-row items-center mb-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-person mr-2"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+            </svg>
+            <p className="mb-0">{comment}</p>
+          </div>
+        ))}
       </div>
     </div>
   ) : null;
 };
 
-export default CommentsModal
+export default CommentsModal;
--- final-project-front-end/src/components/SearchBar.js
+++ final-project-front-end/src/components/SearchBar.js
@@ -1,39 +1,92 @@
-import React from "react";
-// import { SearchIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
 
 const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = () => {
+    setIsLoading(true);
+
+    // Dummy API endpoint
+    const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+    // Make API call
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: searchQuery }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle API response
+        console.log("API response:", data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      });
+  };
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <div className="relative w-full pb-16">
       <input
         type="text"
         placeholder="ex. 2520 Benvenue Ave"
-        className="w-full py-2 pl-3 pr-10 text-gray-700 placeholder-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none focus:border-purple-500 "
+        className="w-full py-2 pl-3 pr-10 text-gray-700 placeholder-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none focus:border-purple-500"
+        value={searchQuery}
+        onChange={handleChange}
       />
       <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-        <button className="bg-yellow-300 hover:bg-yellow-500 -translate-y-8 p-3 rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-search"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-          </svg>
-
-          {/* <div className="h-8 w-8 flex justify-center items-center rounded-full bg-yellow-500 -translate-y-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-search"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-          </svg>
-        </div> */}
+        <button
+          className={`bg-yellow-300 hover:bg-yellow-500 -translate-y-8 p-3 rounded-full ${
+            isLoading && "opacity-50 pointer-events-none"
+          }`}
+          onClick={handleSearch}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <svg
+              className="animate-spin h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zm14-2.582A7.963 7.963 0 0120 12h-4a7.965 7.965 0 01-2.646 5.937l3 2.647zm-6-7.896V4a8.008 8.008 0 015.373 1.987l-3 2.646A3.963 3.963 0 0012 8c-2.206 0-4 1.794-4 4h4c0-1.102.447-2.098 1.17-2.82l3-2.646A7.973 7.973 0 0112 4z"
+              ></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"
+              />
+            </svg>
+          )}
         </button>
       </div>
     </div>
@@ -41,3 +94,6 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
+
+
+//last thing changed before commit, if broken comment out the API endpoint
