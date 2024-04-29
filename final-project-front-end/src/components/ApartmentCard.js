bb456092e06929b026ffc8b45739d56eea7e003d
Josh-Kwong <joshuakwong3@gmail.com>
2024-04-29 16:56:11 -0700

    Finalized the example apartments cards at the bottom

--- final-project-front-end/src/components/ApartmentCard.js
+++ final-project-front-end/src/components/ApartmentCard.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import CommentsModal from "./CommentsModal";
 
-const ApartmentCard = ({ picture }) => {
-  // State for like count and like status
+const ApartmentCard = ({ apartment }) => {
   const [likeCount, setLikeCount] = useState(0);
   const [liked, setLiked] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -44,54 +43,41 @@ const ApartmentCard = ({ picture }) => {
       >
         <div className="h-1/2 pt-4 px-4">
           <img
-            src={picture}
-            alt="Your Image"
+            src={apartment.picture}
+            alt="Apartment Image"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="absolute top-[47%] left-0 w-full bg-white bg-opacity-75 p-4">
           <h2 className="text-center text-black font-extrabold pb-4">
-            2520 Benvenue Ave
+            {apartment.address}
           </h2>
           <h4 className="text-gray-400 text-sm font-medium text-center pb-5">
-            See what people have to say ?
+            See what people have to say?
           </h4>
-          <p className="pl-6 mb-2">
-            <span className="font-bold">John Doe: </span>Dis apartment sick as
-            heck
-          </p>
+          {apartment.comments.map((comment, index) => (
+            <p key={index} className="pl-6 mb-2">
+              <span className="font-bold">{comment.user}: </span>
+              {comment.text}
+            </p>
+          ))}
         </div>
       </div>
 
       {isDialogOpen && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="relative w-1/3 h-11/12 bg-white rounded-lg shadow-lg">
-            {/* Close button */}
             <button
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
               onClick={toggleDialog}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              Close
             </button>
 
-            {/* Dialog content */}
             <div className="p-7">
               <img
-                src={picture}
-                alt="Your Image"
+                src={apartment.picture}
+                alt="Apartment Image"
                 className="w-full h-10/12 object-cover align-middle"
               />
               <div className="flex flex-row justify-between">
@@ -112,38 +98,17 @@ const ApartmentCard = ({ picture }) => {
               </div>
               <div className="border-t border-gray-300 mt-4 pt-4 flex justify-between items-center">
                 <div className="w-1/2 border-r border-gray-300 pr-2 text-center flex items-center justify-center">
-                  {/* Like button */}
                   <button
                     onClick={handleLike}
                     className={`text-blue-500 hover:text-blue-600 flex items-center ${
                       liked && "text-blue-600"
                     }`}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-hand-thumbs-up mr-1"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
-                    </svg>
                     Like
                   </button>
                 </div>
                 <div className="w-1/2 pl-2 text-center flex items-center justify-center">
                   <button className="text-blue-500  hover:text-blue-600 flex items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-chat mr-1"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
-                    </svg>
                     Comment
                   </button>
                 </div>
@@ -161,16 +126,7 @@ const ApartmentCard = ({ picture }) => {
                   className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-full items-end"
                   onClick={handleCommentSubmit}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-send"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
-                  </svg>
+                  Send
                 </button>
               </div>
               <div className="pt-4">
--- final-project-front-end/src/components/ApartmentGrid.js
+++ final-project-front-end/src/components/ApartmentGrid.js
@@ -1,95 +1,153 @@
-import React, { useEffect, useState } from "react";
-import ApartmentCard from "./ApartmentCard"; // Assuming your ApartmentCard component is imported here
-import apartment from "../images/default_apartment_img.jpg";
-import Enclave from "../images/berkeley_enclave.jpg";
+import React, { useState } from "react";
+import ApartmentCard from "./ApartmentCard";
 import SearchBar from "./SearchBar";
+import apartment1 from "../images/default_apartment_img.jpg";
+import apartment2 from "../images/default.jpeg";
+import apartment3 from "../images/default2.jpeg";
+import apartment4 from "../images/default3.webp";
+import apartment5 from "../images/default4.jpeg";
+import apartment6 from "../images/default5.jpeg";
 
 function ApartmentGrid() {
-  const [apartments, setApartments] = useState([]);
   const [history, setHistory] = useState("");
-  console.log(history);
+  const [showPopularSearches, setShowPopularSearches] = useState(false);
 
-  //   useEffect(() => {
-  //     // Fetch apartments from backend when the component mounts
-  //     fetchApartments();
-  //   }, []);
-
-  //   const fetchApartments = async () => {
-  //     try {
-  //       // Fetch apartments data from the backend
-  //       const response = await fetch('YOUR_BACKEND_ENDPOINT');
-  //       if (!response.ok) {
-  //         throw new Error('Failed to fetch data');
-  //       }
-  //       const data = await response.json();
-  //       setApartments(data); // Update state with fetched apartments data
-  //     } catch (error) {
-  //       console.error('Error fetching apartments:', error);
-  //     }
-  //   };
+  const apartments = [
+    {
+      id: 1,
+      picture: apartment1,
+      address: "123 Main St",
+      comments: [{ user: "John Doe", text: "Nice apartment!" }],
+    },
+    {
+      id: 2,
+      picture: apartment2,
+      address: "456 Elm St",
+      comments: [{ user: "Jane Smith", text: "Great location!" }],
+    },
+    {
+      id: 3,
+      picture: apartment3,
+      address: "789 Oak St",
+      comments: [{ user: "Alice Johnson", text: "Beautiful view!" }],
+    },
+    {
+      id: 4,
+      picture: apartment4,
+      address: "1011 Pine St",
+      comments: [{ user: "Bob Williams", text: "Spacious living room!" }],
+    },
+    {
+      id: 5,
+      picture: apartment5,
+      address: "1213 Cedar St",
+      comments: [{ user: "Emily Brown", text: "Love the kitchen!" }],
+    },
+    {
+      id: 6,
+      picture: apartment6,
+      address: "1415 Maple St",
+      comments: [{ user: "Michael Davis", text: "Cozy bedroom!" }],
+    },
+  ];
 
   const handleClick = async () => {
     const apiUrl = "http://127.0.0.1:8000/members/";
-    fetch(apiUrl, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle API response
-        setHistory(data.output);
-        console.log("API response:", data);
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
+      const data = await response.json();
+      setHistory(data.output);
+      setShowPopularSearches(!showPopularSearches); // Toggle popular searches visibility
+      console.log("API response:", data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
   return (
-    <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center relative"
-      style={{ backgroundImage: `url(${Enclave})` }}
-    >
-      <div className="absolute inset-0 bg-gradient-to-t from-yellow-300 to-transparent z-0"></div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 relative z-10">
-        <h1 className="text-5xl font-bold mb-8 py-16 text-white">
-          Start Reviewing, Browsing, or Searching !
-        </h1>
-        <SearchBar />
-        <div className="flex gap-5 mb-12">
+    <div style={styles.container}>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <h1 style={styles.heading}>Start Reviewing, Browsing, or Searching!</h1>
+        <div style={styles.searchBar}>
+          <SearchBar />
           <button
             className="bg-white p-2 rounded-full font-semibold hover:scale-[1.05] duration-200"
             onClick={handleClick}
           >
-            {" "}
-            Search History
+            Popular Searches
           </button>
-          {history && (
-            <h1 className="bg-white p-2 rounded-full font-semibold">
-              {history}
-            </h1>
-          )}
         </div>
-
-        <div className="grid grid-cols-3 gap-x-32 gap-y-32">
-          {/* Iterate over apartments array and render ApartmentCard for each
-          {apartments.map((apartment, index) => (
-            <ApartmentCard key={index} {...apartment} />
-          ))} */}
-          <ApartmentCard picture={apartment} />
-          <ApartmentCard picture={apartment} />
-          <ApartmentCard picture={apartment} />
-          <ApartmentCard picture={apartment} />
-          <ApartmentCard picture={apartment} />
-          <ApartmentCard picture={apartment} />
+        {showPopularSearches && ( // Conditionally render popular searches box
+          <div style={styles.popularSearches}>
+            <h2>Popular Searches</h2>
+            <p>{history}</p>
+          </div>
+        )}
+        <div style={styles.grid}>
+          {apartments.map((apartment) => (
+            <ApartmentCard key={apartment.id} apartment={apartment} />
+          ))}
         </div>
-        <div className="mt-8 flex justify-center">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            Load More Apartments
-          </button>
+        <div style={styles.loadMore}>
+          <button style={styles.button}>Load More Apartments</button>
         </div>
       </div>
     </div>
   );
 }
+
+const styles = {
+  container: {
+    minHeight: "100vh",
+    backgroundColor: "#f7fafc",
+  },
+  heading: {
+    fontSize: "3rem",
+    fontWeight: "bold",
+    marginBottom: "2rem",
+    paddingTop: "4rem",
+    color: "#4a5568",
+    textAlign: "center",
+  },
+  searchBar: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: "1rem",
+  },
+  popularSearches: {
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    padding: "1rem",
+    marginTop: "1rem",
+    backgroundColor: "#fff",
+  },
+  grid: {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gap: "2rem",
+  },
+  loadMore: {
+    marginTop: "2rem",
+    display: "flex",
+    justifyContent: "center",
+  },
+  button: {
+    padding: "0.75rem 1.5rem",
+    backgroundColor: "#4299e1",
+    color: "white",
+    borderRadius: "0.25rem",
+    border: "none",
+    cursor: "pointer",
+    transition: "background-color 0.3s ease",
+  },
+  buttonHover: {
+    backgroundColor: "#3182ce",
+  },
+};
 
 export default ApartmentGrid;
